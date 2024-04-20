5a2d5f5a9dbdb3b46a508bdb4c2c6392cfe59170
Iman <iman.osama2000@gmail.com>
2024-04-20 07:24:31 +0200

    [feat]: Book validation

--- src/main/java/com/example/library/controller/BookController.java
+++ src/main/java/com/example/library/controller/BookController.java
@@ -4,6 +4,7 @@ import com.example.library.dto.BookDto;
 import com.example.library.mapper.Mapper;
 import com.example.library.model.Book;
 import com.example.library.service.BookService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,13 +34,13 @@ public class BookController {
         return new ResponseEntity<>(this.bookService.getById(id), HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity<BookDto> addBook(@RequestBody BookDto bookDto) {
+    public ResponseEntity<BookDto> addBook(@Valid @RequestBody BookDto bookDto) {
         Book book = this.bookMapper.mapFromDto(bookDto);
         Book savedBook = this.bookService.save(book);
         return new ResponseEntity<>(this.bookMapper.mapToDto(savedBook), HttpStatus.CREATED);
     }
     @PutMapping("/{id}")
-    public ResponseEntity<BookDto> updateBook(@PathVariable Long id, @RequestBody BookDto bookDto) {
+    public ResponseEntity<BookDto> updateBook(@PathVariable Long id, @Valid @RequestBody BookDto bookDto) {
         if(!this.bookService.isExists(id)){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
